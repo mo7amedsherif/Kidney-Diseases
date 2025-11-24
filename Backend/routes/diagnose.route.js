99addeb5891d323eeebcf06cc2ada79a1951ab32
mo7amedsherif <160414123+mo7amedsherif@users.noreply.github.com>
2025-11-24 03:43:46 +0200

    edit on code

--- Backend/routes/diagnose.route.js
+++ Backend/routes/diagnose.route.js
@@ -7,8 +7,11 @@ const { diagnoseDisease } = require('../controller/diagnose.controller');
 // Import authentication middleware
 const { verifyToken } = require('../middleware/rouleStatus.middleware');
 
+// Import validation middleware
+const { validateDiagnosisInput } = require('../middleware/diagnose.validation');
+
 // POST route for disease diagnosis (protected)
-router.post('/', verifyToken, diagnoseDisease);
+router.post('/', verifyToken, validateDiagnosisInput, diagnoseDisease);
 
 module.exports = router;
 
